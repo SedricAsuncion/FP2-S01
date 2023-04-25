d58abb84794ad9561b003eaa280dc7d65571ddab
SedricAsuncion <128073299+SedricAsuncion@users.noreply.github.com>
2023-04-25 10:39:32 -0600

    Add files via upload

--- SedricAsuncion_ExternalFiles.py
+++ SedricAsuncion_ExternalFiles.py
@@ -4,11 +4,21 @@
 #-----------------------------
 
 ## ----- Import ----- ##
+import os
 
 ## ----- Functions ----- ##
 def file_create():
-    with open('Player_Data.txt', 'w') as created:
-        created.write['User: ']
+    Player = input("Create Character Name: ")
+    Race = input('Create Character Race: ')
+    Job = input('Create Character Job: ')
+    Level = 0
+    with open(f"{Player}_Data.txt", 'w') as created:
+        created.write(f'User {Player}')
+        created.write(f'\nRace: {Race}')
+        created.write(f'\nJob: {Job}')
+        created.write(f'\nLevel: {Level}')
 
 def main():
     file_create()
+
+main()
